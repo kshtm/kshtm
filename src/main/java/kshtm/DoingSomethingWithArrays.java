844e3f73927ee9b96d9dd73e70510ab0d862cb27
kshtm <kshtm84@gmail.com>
2018-01-18 21:25:04 +0300

    Ищем максималное значение в массиве

--- src/main/java/kshtm/DoingSomethingWithArrays.java
+++ src/main/java/kshtm/DoingSomethingWithArrays.java
@@ -2,7 +2,7 @@ package kshtm;
 
 public class DoingSomethingWithArrays {
 
-    public static int serchMaxElement(int[] arr) {
+    public static int searchMaxElement(int[] arr) {
 
         int max = arr[0];
         for(int i = 0; i < arr.length; i++) {
@@ -16,6 +16,6 @@ public class DoingSomethingWithArrays {
     public static void main(String[] args) {
 
         int [] intArr = {4, 5, 10, 100, 40, 88, 1500, -15, 87, 999, 5000};
-        System.out.println(serchMaxElement(intArr));
+        System.out.println(searchMaxElement(intArr));
     }
 }
